49b3942954661ddfe1702e67aa9a058fb2c57611
hardik <160777787+hpatel1101@users.noreply.github.com>
2026-04-08 20:39:44 -0400

    Add type declaration for app layout

---
+++ app/layout.d.ts
@@ -0,0 +1,7 @@
+declare const metadata: {
+  title: string;
+  description: string;
+};
+
+export { metadata };
+export default function RootLayout(props: { children: React.ReactNode }): React.ReactElement;
